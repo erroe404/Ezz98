e6fe9e20d6a2496729d7aae69663d7191d97bc50
aboalezz atef <aboalezz.atef12@yahoo.com>
2022-02-24 19:38:49 +0200

    first commit

---
+++ js/custom.js
@@ -0,0 +1,31 @@
+
+$(document).ready(function () {
+    $(window).scroll(function () {
+        if ($(this).scrollTop() > 100) {
+            $('#back-to-top').fadeIn();
+        } else {
+            $('#back-to-top').fadeOut();
+        }
+    });
+    $('#back-to-top').click(function () {
+        $("html, body").animate({scrollTop: 0}, 600);
+        return false;
+    });
+
+
+    $("#toggle").click(function () {
+        $(this).toggleClass("on");
+        $("#menu").slideToggle();
+    });
+
+});
+
+
+
+
+
+
+
+
+
+
